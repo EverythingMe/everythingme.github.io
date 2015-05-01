068c8572b59af9abc1bf6848d8647f8e730a21b2
Ran Ben Aharon <ran@ranbena.com>
2015-05-01 10:20:56 +0300

    Revealing footer content only after projects loaded

--- static/evme.js
+++ static/evme.js
@@ -1,21 +1,26 @@
-(function() {
-var url = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&output=json&callback=?&q='
-var feed_url = 'http://geeks.everything.me/feed';
-
-$.getJSON(url+encodeURIComponent(feed_url)).done(function(data) {
-  // up to 4 items
-  var items = {'entries': data.responseData.feed.entries.slice(0,3)};
-
-  var source   = $('#blog-template').html();
-  var template = Handlebars.compile(source);
-  var html = template(items);
-
-  $('#blog').append(html);
+$('#projects').on('mixEnd', function(e, state){
+  $('footer')[0].dataset.ready = 'true';
+  loadBlogPosts();
+  loadJobs();
 });
 
-})();
+function loadBlogPosts() {
+  var url = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&output=json&callback=?&q='
+  var feed_url = 'http://geeks.everything.me/feed';
 
-(function() {
+  $.getJSON(url+encodeURIComponent(feed_url)).done(function(data) {
+    // up to 3 items
+    var items = {'entries': data.responseData.feed.entries.slice(0,3)};
+
+    var source   = $('#blog-template').html();
+    var template = Handlebars.compile(source);
+    var html = template(items);
+
+    $('#blog').append(html);
+  });
+}
+
+function loadJobs() {
   var apiUrl = 'https://api.import.io/store/data/b76794af-c9cb-4d26-bc5f-45966514f6da/_query'+
               '?input/webpage/url='+encodeURIComponent('http://everything.me/jobs')+
               '&_user=f3e9d602-9b6e-4492-bceb-4f0e2caa96f3'+
@@ -28,4 +33,4 @@ $.getJSON(url+encodeURIComponent(feed_url)).done(function(data) {
 
     $('#jobs').append(html);
   });
-})();
+}
